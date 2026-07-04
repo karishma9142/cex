32670ef75ead25fe82ea8743478e75a543c41a48
karishma9142 <karishmacair47050@gmail.com>
2026-07-05 00:11:02 +0530

    delet market page

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import RegisterPage from './pages/RegisterPage'
 import './index.css'
 import LandingPage from './pages/LandingPage'
 import { AuthProvider } from './context/AuthContext'
+import MarketPage from './pages/MarketPage'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -15,6 +16,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         <Route path='/' element = {<LandingPage/>}/>
         <Route path="/login"    element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
+        {/* <Route path='/market' element={<MarketPage/>}/> */}
         {/* <Route path="*"         element={<Navigate to="/login" replace />} /> */}
       </Routes>
     </BrowserRouter>
